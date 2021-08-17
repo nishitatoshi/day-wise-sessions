091cabb9a5b5e12c948b681220c3ca28c7c4b893
Nishita Toshi <74404458+nishitatoshi@users.noreply.github.com>
2021-08-18 00:28:27 +0530

    Add files via upload

---
+++ day10/src/com/techment/day10/ConnectionProperty.java
@@ -0,0 +1,8 @@
+package com.techment.day10;
+
+public interface ConnectionProperty {
+	 public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	 public static final String URL = "jdbc:mysql://localhost:3306/techment?autoReconnect=true&useSSL=false";
+	 public static final String USERNAME = "root";
+	 public static final String PASSWORD = "123456";
+	}
